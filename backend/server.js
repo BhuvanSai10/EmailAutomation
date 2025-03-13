df05bf1da10560bed60ab0c014534492ff52b857
BhuvanSai10 <bhuvansai012345@gmail.com>
2025-03-13 16:31:38 +0530

    final commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use('/api', routes); 
 
 schedule.scheduleJob('*/1 * * * *', sendScheduledEmails);
